b686b2c67cbc03d6ba7ec4eda7512012c3471bd1
Martina <89107197+8Martina8@users.noreply.github.com>
2023-08-02 16:56:34 +0300

    added recyclerView

--- app/src/main/java/com/reciply/HomeFragment.kt
+++ app/src/main/java/com/reciply/HomeFragment.kt
@@ -5,12 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.navArgs
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.reciply.R
+import com.reciply.models.Recipe
 
 
 class HomeFragment : Fragment() {
+    val data: List<Recipe> = arrayListOf(
+    )
 
-
+    private lateinit var rv: RecyclerView
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -19,4 +25,13 @@ class HomeFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        rv = view.findViewById(R.id.rv_home)
+        rv.adapter= RecipeItemAdapter(data)
+        rv.layoutManager = LinearLayoutManager(activity?.applicationContext, RecyclerView.HORIZONTAL, false)
+
+    }
 }
---
+++ app/src/main/java/com/reciply/RecipeItemAdapter.kt
@@ -0,0 +1,60 @@
+package com.reciply
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.example.reciply.R
+import com.reciply.models.Recipe
+
+class RecipeItemAdapter(val data:List<Recipe>) : RecyclerView.Adapter<RecipeItemAdapter.MyViewHolder>() {
+    private lateinit var context: Context
+    var onItemClick: ((Recipe)->Unit)?=null
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val row= LayoutInflater.from(parent.context).inflate(R.layout.home_recipe_item,parent,false)
+        return MyViewHolder(row)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+
+        holder.txtRecipeTitle.text=data[position].strMeal
+        //holder.txtViewDescription.text=data[position].description
+        Glide.with(context)
+            .load(data[position].strMealThumb)
+            .apply(
+                RequestOptions()
+                .placeholder(R.drawable.baseline_image_24)
+                .error(R.drawable.baseline_broken_image_24))
+            .into(holder.imgView)
+        holder.itemView.setOnClickListener {
+
+            onItemClick?.invoke(data[position])
+        }
+
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
+
+    class MyViewHolder(row: View): RecyclerView.ViewHolder(row){
+        var txtRecipeTitle=row.findViewById<TextView>(R.id.txt_rec_title)
+        //var txtViewDescription=row.findViewById<TextView>(R.id.txtViewDescription)
+        var imgView=row.findViewById<ImageView>(R.id.img_rec_thumb)
+
+    }
+
+
+
+    override fun onAttachedToRecyclerView(recyclerView: RecyclerView) {
+        super.onAttachedToRecyclerView(recyclerView)
+        context = recyclerView.context
+    }
+}
