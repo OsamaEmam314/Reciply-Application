6397777b030a98a2a13ee9c5210bf5b2f2a1ff78
Martina <89107197+8Martina8@users.noreply.github.com>
2023-08-02 16:39:15 +0300

    Revert "Merge pull request #3 from OsamaEmam314/ApiService"
    This reverts commit 74f82f97e29ac350c6520ff26847463472e299cf, reversing
    changes made to 8154a96d70e81b31c966752df840a73430279cad.

--- app/src/main/java/com/reciply/AuthActivity.kt
+++ app/src/main/java/com/reciply/AuthActivity.kt
@@ -2,18 +2,11 @@ package com.reciply
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.get
 import com.example.reciply.R
-import com.reciply.network.ServiceViewModel
 
 class AuthActivity : AppCompatActivity() {
-     lateinit var viewModel: ServiceViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//        viewModel= ViewModelProvider(this).get(ServiceViewModel::class.java)
-//        viewModel.getMealById()
     }
 }
