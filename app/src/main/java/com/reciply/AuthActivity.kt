06618ff651c2bd2b7636996e26eea3f9519ce759
OsamaEmam314 <osamakhaled31415@gmail.com>
2023-08-04 15:46:18 +0300

    loigin backcommit

--- app/src/main/java/com/reciply/AuthActivity.kt
+++ app/src/main/java/com/reciply/AuthActivity.kt
@@ -3,31 +3,34 @@ package com.reciply
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import com.example.reciply.R
 import com.reciply.viewmodel.MealViewModel
 
 @Suppress("DEPRECATION")
 class AuthActivity : AppCompatActivity() {
      //lateinit var viewModel: MealViewModel
+     private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //viewModel= ViewModelProvider(this).get(MealViewModel::class.java)
         //viewModel.getMealById("a")
         //viewModel.getRandomMeal()
+         val navHostFragment = supportFragmentManager.findFragmentById(R.id.AuthNav_Host) as NavHostFragment
+         navController = navHostFragment.navController
     }
 
     override fun onBackPressed() {
-        val navController=findNavController(R.id.AuthNav_Host)
-        val currentDestination = navController.currentDestination?.id
         val loginFragment = R.id.loginFragment2
-        if(currentDestination==loginFragment)
-        {
+        if (navController.currentDestination?.id == loginFragment) {
             finish()
-        }
-        else{
-            super.onBackPressed()
+        } else {
+
+            navController.navigateUp()
         }
     }
 }
