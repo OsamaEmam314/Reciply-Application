b11b17f932ae936005744c6270753d7c0da33cdd
Mona Adel <96449268+MonaAdel2@users.noreply.github.com>
2023-08-04 19:08:15 +0300

    change the function name

--- app/src/main/java/com/reciply/viewmodel/MealViewModel.kt
+++ app/src/main/java/com/reciply/viewmodel/MealViewModel.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.reciply.data.Meal
+import com.reciply.network.ApiClient
 import com.reciply.repo.MealsRepository
 import kotlinx.coroutines.launch
 
@@ -13,8 +14,8 @@ class MealViewModel(val mealsRepository: MealsRepository):ViewModel() {
     private val _MealListBYName= MutableLiveData<List<Meal>>()
     val MealListBYNmae: LiveData<List<Meal>> =_MealListBYName
     private val randomMeal=MutableLiveData<Meal?>()
-/*
-    fun getMealById(Name:String){
+
+    fun getMealByName(Name:String){
         viewModelScope.launch {
             val response= ApiClient.getMealByName(Name)
             Log.d("asd->", "listofMeals:$response ")
@@ -22,7 +23,7 @@ class MealViewModel(val mealsRepository: MealsRepository):ViewModel() {
 
         }
     }
-*/
+
 
 
     fun getRandomMeal(){
