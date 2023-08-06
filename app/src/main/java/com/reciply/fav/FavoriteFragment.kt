9bcb1176c1eb9455e91154f7eab1c528c549c156
Mona Adel <96449268+MonaAdel2@users.noreply.github.com>
2023-08-06 22:00:41 +0300

    the alert dialog ui is added

--- app/src/main/java/com/reciply/fav/FavoriteFragment.kt
+++ app/src/main/java/com/reciply/fav/FavoriteFragment.kt
@@ -1,10 +1,15 @@
 package com.reciply.fav
 
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.Window
+import android.widget.Button
 import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
@@ -46,6 +51,26 @@ class FavoriteFragment : Fragment() {
 //        recyclerFav.layoutManager = GridLayoutManager(requireContext(), 2)
 
 
+    }
+
+    private fun showCustomDialog(){
+        val dialog = Dialog(requireContext())
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setCancelable(false)
+        dialog.setContentView(R.layout.custom_dialog)
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+        val btnDelete: Button = dialog.findViewById(R.id.btn_delete_dialog)
+        val btnCancel: Button = dialog.findViewById(R.id.btn_cancel_dialog)
+
+        btnDelete.setOnClickListener {
+            // delete recipe from db
+        }
+        btnCancel.setOnClickListener {
+            // hide the dialog
+            dialog.dismiss()
+        }
+        dialog.show()
     }
 
 }
